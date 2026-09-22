23e8546f46a926a324b7a7e957a7a93b5509ef1b
Jaohar Raihan <jaoharraihan@Jaohars-MacBook-Pro.local>
2026-09-23 01:44:44 +0600

    added fb

---
+++ js/firebase-config.js
@@ -0,0 +1,13 @@
+// Firebase Web App configuration
+// App ID: 1:996374615120:web:c7aa1ae99bc42c351fad55
+// Project: cutbook-47881
+export const firebaseConfig = {
+  projectId: "cutbook-47881",
+  appId: "1:996374615120:web:c7aa1ae99bc42c351fad55",
+  databaseURL: "https://cutbook-47881-default-rtdb.firebaseio.com",
+  storageBucket: "cutbook-47881.firebasestorage.app",
+  apiKey: "AIzaSyCIuJw4lpFuOs8uhGHPmy9CjDSe-uDXvYE",
+  authDomain: "cutbook-47881.firebaseapp.com",
+  messagingSenderId: "996374615120",
+  measurementId: "G-8EHB78RF4C"
+};
